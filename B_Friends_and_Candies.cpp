fa583e54a51b6f9864ce3a09e4f0758feb8b0005
Rahul <malakarahul111@gmail.com>
2022-11-21 00:09:34 +0530

    nothing

---
+++ B_Friends_and_Candies.cpp
@@ -0,0 +1,59 @@
+//RAHUL MALAKAR 2112022
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+
+#define sp setprecision
+#define eb emplace_back
+
+#define vi vector<int>
+#define vll vector<long long>
+#define si set<int>
+#define sll set<int>
+
+
+#define forl(i,n) for(int i=0; i<n; i++)
+#define forle(i,n) for(int i=0; i<=n; i++)
+
+#define sorti(v) sort(v.begin(), v.end())
+
+
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+
+    ll t;
+    cin >> t;
+    forl(i,t)
+    {
+        int a; cin>>a;
+        vi v;
+        int sum=0;
+        forl(i,a){
+            int b; cin>>b;
+            v.eb(b);
+            sum+=b;
+        }
+        int ans=0;
+        if(sum%v.size()!=0){
+            cout<<-1<<endl;
+            continue;
+        }
+        else{
+            for(auto it:v){
+                if(it>(sum/v.size())){
+                    ans++;
+                }
+            }
+        }
+        cout<<ans<<endl;
+        
+
+    }
+
+    return 0;
+}
