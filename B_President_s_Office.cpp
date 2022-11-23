88dd32ed710fe7c0bed07db181f3acd36339d67d
Rahul <malakarahul111@gmail.com>
2022-11-23 20:12:33 +0530

    push karo

--- B_President_s_Office.cpp
+++ B_President_s_Office.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
- 
+
 int arr[30];
 int ar[110][110];
  
@@ -16,12 +16,14 @@ int main() {
             else ar[i][k] = c-'A'+1;
         }
     }
+
     for (int i=1; i<=N; i++) {
         for (int k=1; k<=M; k++) {
             if (ar[i][k] != 0 && ar[i][k] != pres && (ar[i-1][k] == pres || ar[i+1][k] == pres
                 || ar[i][k-1] == pres || ar[i][k+1] == pres) ) arr[ ar[i][k] ] = 1;
         }
     }
+    
     int resp = 0;
     for (int i=1; i<=26; i++) resp += arr[i];
     cout<<resp;
