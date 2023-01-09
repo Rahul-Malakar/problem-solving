22467cda493192406e9602af8b3d912b99f28c26
Rahul <malakarahul111@gmail.com>
2023-01-09 23:58:02 +0530

    commit

--- C_Permutation_Operations.cpp
+++ C_Permutation_Operations.cpp
@@ -1,17 +1,47 @@
-#include<bits/stdc++.h>
+//RAHUL MALAKAR 2112022
+
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+#define ll long long
+#define ld long double
+
+#define sp setprecision
+#define eb emplace_back
+
+#define vi vector<int>
+#define vll vector<long long>
+#define si set<int>
+#define sll set<int>
+
+#define sorti(v) sort(v.begin(), v.end())
 
 
-    int t; cin>>t;
-    for(int i=0; i<t; i++){
+int main()
+{
+    
+    ios_base::sync_with_stdio(false);
+
+    ll t;
+    cin >> t;
+    while(t--)
+    {
         int a; cin>>a;
-        int arr[a];
+        vll v;
         for(int i=0; i<a; i++){
-            cin>>arr[i];
+            int b; cin>>b;
+            v.eb(b);
         }
-        
+        vector<pair<int,int>> m;
+        for(int i=0; i<a-1; i++){
+            m.eb(v[i]-v[i+1],i);
+        }
+        sort(m.begin(), m.end());
+        cout<<1;
+        for(auto it:m){
+            cout<<" "<<it.second +2;
+        }
+        cout<<endl;
     }
 
     return 0;
