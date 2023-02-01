aeacad390935171372237e1c8755f15cea373085
Rahul <malakarahul111@gmail.com>
2023-02-01 19:15:12 +0530

    commit

--- B_Decoding.cpp
+++ B_Decoding.cpp
@@ -3,45 +3,36 @@ using namespace std;
 
 int main()
 {
+    int a; cin>>a;
+    string s; cin>>s;
 
-    ios_base::sync_with_stdio(false);
-
-    long long t;
-    cin >> t;
-    string s;
-    cin >> s;
-    string news;
-    if (t % 2 == 0)
-    {
-        int ileft = t - 2;
-        int iright = 1;
-        while (ileft >= 0)
-        {
-            news.push_back(s[ileft]);
-            ileft -= 2;
+    string news(a,'0');
+    int l=a/2 -1, r=a/2;
+    for(int i=0; i<a; i++){
+        if(a&1){
+            
+            if(i%2==0){
+                news[r]=s[i];
+                r++;
+            }
+            else{
+                news[l]=s[i];
+                l--;
+            }
         }
-        while (ileft < 0 && iright <= t - 1)
-        {
-            news.push_back(s[iright]);
-            iright += 2;
+        else{
+            if(i%2==0){
+                news[l]=s[i];
+                l--;
+            }
+            else{
+                
+                news[r]=s[i];
+                r++;
+            }
         }
     }
-    else
-    {
-        int ileft = t - 2;
-        int iright = 0;
-        while (ileft >= 0)
-        {
-            news.push_back(s[ileft]);
-            ileft -= 2;
-        }
-        while (ileft < 0 && iright <= t - 1)
-        {
-            news.push_back(s[iright]);
-            iright += 2;
-        }
-    }
-    cout << news;
+    cout<<news<<endl;
 
     return 0;
 }
