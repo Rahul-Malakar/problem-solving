49832556d1bfb5d7ac26bf52cc79696f242dd7d1
Rahul <malakarahul111@gmail.com>
2023-03-26 21:30:48 +0530

    com

--- B_Frog_2.cpp
+++ B_Frog_2.cpp
@@ -52,25 +52,14 @@ int main()
 
     // cout<<solve(v, dp , n, k);
     
-    // dp[0] = 0;
-    // for (int i = 1; i < n; i++) {
-    //     for (int j = 1; j <= k; j++) if (i-j >= 0){
-    //         dp[i] = min(dp[i], dp[i-j] + abs(v[i] - v[i-j]));
-    //     }
-    // }
-    // cout<<dp[n-1];
-
-    dp[0]=0;
-    for(int i=1; i<n; i++){
-        for(int j=1; j<=k; j++){
-            if(i-j>=0){
-                dp[i]=min(dp[i], dp[i-j] + abs(v[i]-v[i-j]));
-            }
-            
+    dp[0] = 0;
+    for (int i = 1; i < n; i++) {
+        for (int j = 1; j <= k; j++) if (i-j >= 0){
+            dp[i] = min(dp[i], dp[i-j] + abs(v[i] - v[i-j]));
         }
-         
     }
     cout<<dp[n-1];
+
 
     return 0;
 }
